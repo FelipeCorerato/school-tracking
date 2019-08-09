883416eb66a2a3e015edbec181ac6568c12784ed
FelipeCorerato <fcorerato@gmail.com>
2019-08-09 08:58:59 -0300

    Gitinore working and Menu design started
    :carousel_horse:

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,14 +2,48 @@ import React from 'react';
 import {
     View,
     SafeAreaView,
+    TouchableOpacity,
+    Text,
     StyleSheet
-} from 'react-native'
+} from 'react-native';
+import firebase from 'firebase';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default class Menu extends React.Component {
+    logout = () => {
+        // await firebase.auth().signOut();
+        // await AsyncStorage.clear();
+
+        this.props.navigation.navigate('Splash');
+    }
+
     render() {
         return(
             <SafeAreaView style={styles.container}>
+                <View style={styles.header}>
+                    <View style={styles.userData}>
 
+                    </View>
+
+                    <View style={styles.fastOptions}>
+                        <TouchableOpacity>
+                            <Text>Sair</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity>
+                            <Text>Ajustes</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity>
+                            <Text>Info</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View style={styles.footer}>
+
+                </View>               
             </SafeAreaView>
         );
     }
@@ -18,5 +52,21 @@ export default class Menu extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+
+    header: {
+        backgroundColor: '#DCB747'
+    },
+
+    userData: {
+        flexDirection: 'row'
+    },
+
+    fastOptions: {
+        flexDirection: 'row'
+    },
+
+    footer: {
+
     }
 });
